20753fee638e3762ff0236a8a510ff7c5bd1c33b
Aker120112 <ezekieltaiwo232@gmail.com>
2023-12-28 07:41:26 -0800

    hand testing for the shell project

---
+++ my_code.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - prints to standard output
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("this shell project is a testament");
+	printf(" to our profound knowledge in c programming\n");
+	return (0);
+}
